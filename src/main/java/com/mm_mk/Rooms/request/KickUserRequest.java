fdee93c69877e06682c729d117b99e04202a7b2c
Mikhael Edman Gomez <edman.gomez.business@gmail.com>
2025-10-24 14:53:41 +0800

    Added validation for kick and mute request dto

--- src/main/java/com/mm_mk/Rooms/request/KickUserRequest.java
+++ src/main/java/com/mm_mk/Rooms/request/KickUserRequest.java
@@ -1,7 +1,10 @@
 package com.mm_mk.Rooms.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
 public record KickUserRequest(
+        @NotBlank
         UUID userId
 ) {}
